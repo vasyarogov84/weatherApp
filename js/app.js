3a838689572995d9b2a5be1361f2a733479b8716
Viktor Grom <viktor@placesscout.com>
2017-09-08 15:25:06 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var http = require('http');
+// http.createServer(function(request,response) {
+//   response.writeHead(200, {'Content-Type':'text/plain'});
+//   setInterval(function() {
+//     response.write(new Date() + "\n");
+//   },1000);
+// }).listen(3000);
+// console.log("Check port:3000");
+
+
+
+
+// var http  = new XMLHttpRequest();
+// var url =  'http://jsonplaceholder.typicode.com/posts/1';
+  
+
+// $.ajax({
+//    url: url,
+//    method: 'GET'
+// }).then(function(data) {
+//   var i = 0;
+//         for(var items in data) {
+//           i++;
+//  console.log("data" +  i + " " + data[items]);
+//         }
+  
+// });
